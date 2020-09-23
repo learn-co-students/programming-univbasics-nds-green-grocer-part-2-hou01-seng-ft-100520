5c08c43a34e01f9ebf65dfdde27a3a9631d382cf
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-23 02:14:06 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,90 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  if coupons.length > 0 then
+    new_cart = [];
+    cart.each do |item|
+      coupons.each do |coupons_item|
+        if item[:item] == coupons_item[:item] then
+          if item[:count] >= coupons_item[:num] then
+            item[:count] -= coupons_item[:num];
+            new_cart.push({
+              :item => item[:item],
+              :price => item[:price],
+              :clearance => item[:clearance],
+              :count => item[:count]
+            })
+            new_cart.push({
+              :item => "#{item[:item]} W/COUPON",
+              :price => coupons_item[:cost]/coupons_item[:num],
+              :clearance => item[:clearance],
+              :count => coupons_item[:num]
+            })
+          else
+            item[:count] = 0;
+            new_cart.push({
+              :item => "#{item[:item]} W/COUPON",
+              :price => coupons_item[:cost]/coupons_item[:num],
+              :clearance => item[:clearance],
+              :count => coupons_item[:num]
+            })
+          end
+        end
+      end
+        found = false
+      
+      cart.each do |cart_item|
+        new_cart.each do |new_cart_item2|
+          if (cart_item[:item] == new_cart_item2[:item]) then
+            found = true
+          end
+        end
+      end
+        if not found then
+          new_cart.push(item);
+        end
+    end
+    return new_cart
+  end
+  return cart
+end
+  
+def apply_clearance(cart)
+  cart.each{
+    |item|
+    if item[:clearance] then
+      new_price = (item[:price] * 8/10).round(2);
+      item[:price] = new_price
+    end
+  }
 end
 
-def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  total = 0;
+
+  consolidated_cart = consolidate_cart(cart);
+  couponds_applied_cart = apply_coupons(consolidated_cart, consolidate_cart(coupons))
+  
+  p couponds_applied_cart
+  
+  final_cart = apply_clearance(couponds_applied_cart);
+  
+  
+  final_cart.each {|item|  total += item[:price] * item[:count]};
+  
+  if total > 100 then
+    total = total * 9/10
+  end
+
+  return total.round(2)
 end
+
+p checkout([
+  {:item=>"SOY MILK", :price=>4.5, :clearance=>true}, 
+  {:item=>"AVOCADO", :price=>3.0, :clearance=>true}, 
+  {:item=>"AVOCADO", :price=>3.0, :clearance=>true}, 
+  {:item=>"CHEESE", :price=>6.5, :clearance=>false}, 
+  {:item=>"CHEESE", :price=>6.5, :clearance=>false}, 
+  {:item=>"CHEESE", :price=>6.5, :clearance=>false}
+  ], [{:item=>"AVOCADO", :num=>2, :cost=>5.0}, {:item=>"CHEESE", :num=>3, :cost=>15.0}])
